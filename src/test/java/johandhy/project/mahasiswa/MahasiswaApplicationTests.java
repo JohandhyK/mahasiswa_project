9769b1e6fe989f2e6430e6f024f3ada098d81c5a
it.support <it.support@HQ009-143.banksinarmas.com>
2023-06-21 08:14:41 +0700

    first commit

---
+++ src/test/java/johandhy/project/mahasiswa/MahasiswaApplicationTests.java
@@ -0,0 +1,13 @@
+package johandhy.project.mahasiswa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MahasiswaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
